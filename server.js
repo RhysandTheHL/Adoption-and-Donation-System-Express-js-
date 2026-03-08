196cd46c6fcfcb58538c4de3d3f8bdcf4547b6fb
Jhyzzee Dianela <jhdianela@my.cspc.edu.ph>
2026-03-08 23:51:07 +0800

    Login

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/landing page/index.html');
 });
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login/user-login.html');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'admin') {
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
